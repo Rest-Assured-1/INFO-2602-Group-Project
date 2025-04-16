cb63f4578584f7d121ae36a6b852a6398745cba9
KellyAnnBoodram <boodram.kellyann@gmail.com>
2025-04-16 11:30:09 -0400

    apartment controller

--- App/controllers/apartment.py
+++ App/controllers/apartment.py
@@ -1,42 +1,47 @@
 from App.models import Apartment
 from App.database import db
 
-def create_apartment(title, body, amenities, photo, pets_allowed, price, address, cityname, landlord_id):
+def create_apartment(data, landlord_id):
     apartment = Apartment(
-        title=title,
-        body=body,
-        amenities=amenities,
-        photo=photo,
-        pets_allowed=pets_allowed,
-        price=price,
-        address=address,
-        cityname=cityname,
+        title=data.get('title'),
+        body=data.get('body'),
+        amenities=data.get('amenities'),
+        photo=data.get('photo'),
+        pets_allowed=data.get('pets_allowed'),
+        price=float(data.get('price')),
+        address=data.get('address'),
+        cityname=data.get('cityname'),
         landlord_id=landlord_id
     )
     db.session.add(apartment)
     db.session.commit()
-
-def get_apartment_by_id(apartment_id):
-    return Apartment.query.get(apartment_id)
+    return apartment
 
 def get_all_apartments():
     return Apartment.query.all()
 
-def update_apartment(apartment_id, title, body, amenities, photo, pets_allowed, price, address, cityname):
-    apartment = Apartment.query.get(apartment_id)
-    if apartment:
-        apartment.title = title
-        apartment.body = body
-        apartment.amenities = amenities
-        apartment.photo = photo
-        apartment.pets_allowed = pets_allowed
-        apartment.price = price
-        apartment.address = address
-        apartment.cityname = cityname
-        db.session.commit()
+def get_apartment_by_id(apartment_id):
+    return Apartment.query.get(apartment_id)
+
+def update_apartment(apartment_id, data):
+    apartment = get_apartment_by_id(apartment_id)
+    if not apartment:
+        return None
+    apartment.title = data.get('title')
+    apartment.body = data.get('body')
+    apartment.amenities = data.get('amenities')
+    apartment.photo = data.get('photo')
+    apartment.pets_allowed = data.get('pets_allowed')
+    apartment.price = float(data.get('price'))
+    apartment.address = data.get('address')
+    apartment.cityname = data.get('cityname')
+    db.session.commit()
+    return apartment
 
 def delete_apartment(apartment_id):
-    apartment = Apartment.query.get(apartment_id)
-    if apartment:
-        db.session.delete(apartment)
-        db.session.commit()
+    apartment = get_apartment_by_id(apartment_id)
+    if not apartment:
+        return False
+    db.session.delete(apartment)
+    db.session.commit()
+    return True
