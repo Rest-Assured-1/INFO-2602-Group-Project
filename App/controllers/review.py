771203003dd5f9bca1dcf384c9650345d28ee7e2
KellyAnnBoodram <boodram.kellyannc@gmail.com>
2025-04-17 19:27:37 -0400

    review controllers

--- App/controllers/review.py
+++ App/controllers/review.py
@@ -1,33 +1,40 @@
 from App.models import Review, Apartment
 from App.database import db
-from flask_login import current_user
 
-def add_review(apartment_id, rating, content):
+def create_review(apartment_id, rating, content, tenant_id):
     apartment = Apartment.query.get(apartment_id)
+
     if apartment:
         new_review = Review(
             content=content,
             rating=rating,
             apartment_id=apartment.id,
-            tenant_id=current_user.id 
+            tenant_id=tenant_id
         )
         db.session.add(new_review)
         db.session.commit()
+        return new_review
 
 
 def get_reviews_for_apartment(apartment_id):
     return Review.query.filter_by(apartment_id=apartment_id).all()
 
+
 def update_review(review_id, rating, content):
     review = Review.query.get(review_id)
-    if review and review.tenant_id == current_user.id:
-        review.rating = rating
-        review.content = content
-        db.session.commit()
+    if not review:
+        return None
+    
+    review.rating = rating
+    review.content = content
+    db.session.commit()
+    return review
+
 
 def delete_review(review_id):
     review = Review.query.get(review_id)
-    if review and review.tenant_id == current_user.id:
-        db.session.delete(review)
-        db.session.commit()
-
+    if not review:
+        return False
+    db.session.delete(review)
+    db.session.commit()
+    return True
